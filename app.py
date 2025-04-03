3f82b8e60d1c50674396510dec6564232751e26b
Kiran Subedi <164513882+Kiransubedi545@users.noreply.github.com>
2025-04-03 11:39:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,1 @@
-📂 concrete-strength-app
-├── app.py                  # Your Streamlit app
-├── lightgbm_model.pkl      # Your saved model
-├── scaler.pkl              # Your saved scaler
-└── requirements.txt        # Dependencies
+
